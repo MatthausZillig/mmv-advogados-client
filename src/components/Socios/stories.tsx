9b5fdc4236310077f76c768cca7101167433737c
Matthaus Zillig <dextra.mzillig@creditas.com>
2021-01-12 22:09:41 -0300

    fix: socios

--- src/components/Socios/stories.tsx
+++
@@ -1,9 +0,0 @@
-import { Story, Meta } from '@storybook/react/types-6-0'
-import Socios from '.'
-
-export default {
-  title: 'Socios',
-  component: Socios
-} as Meta
-
-export const Default: Story = () => <Socios />
